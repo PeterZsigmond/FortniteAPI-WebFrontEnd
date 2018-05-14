e49783b1f390a5e113cf68de5655f744178e39ae
P1nkm4n <zspeti02@gmail.com>
2018-05-14 20:38:30 +0200

    cache players / new colors

--- fortnite.js
+++ fortnite.js
@@ -1,15 +1,19 @@
 ﻿var keys = ["LOrmlUBVcdcOCzhxd5QM", "i9GNFvuGLRX3Xuqgiyfk"];
 var key;
 
+createCookiesIfNotExists();
+printLastPlayers();
+
 switch(Math.floor((Math.random() * 2) + 1))
 {
 	case 1: key = keys[0]; break;
 	case 2: key = keys[1]; break;
 }
 
-function updatePlayer()
+function updatePlayer(nev)
 {
-	var nev = $("#formName").val();
+	if(nev == "")
+		nev = $("#formName").val();
 	
 	if(!nev.trim())
 		return;
@@ -24,9 +28,12 @@ function updatePlayer()
 		},
         success: function(result)
 		{
+			addLastPlayer(result.displayName);
+			printLastPlayers();
+			
 			$("#errorMessage").html("");
 			
-			var profilePic = Math.floor((Math.random() * 8) + 1);
+			var profilePic = Math.floor((Math.random() * 7) + 1);
 			
 			if(result.displayName == "Ninja")
 				profilePic = "ninja";
@@ -48,11 +55,6 @@ function updatePlayer()
 			$("#duo_lastMatch").html("Utolsó meccs: " + timeSince(Date.parse(result.br.stats.pc.duo.lastMatch)));
 			$("#squad_lastMatch").html("Utolsó meccs: " + timeSince(Date.parse(result.br.stats.pc.squad.lastMatch)));
 			
-			$("#solo_minutesPlayed").html("Játszott idő: " + Math.floor(result.br.stats.pc.solo.minutesPlayed / 60) + " óra");
-			$("#duo_minutesPlayed").html("Játszott idő: " + Math.floor(result.br.stats.pc.duo.minutesPlayed / 60) + " óra");
-			$("#squad_minutesPlayed").html("Játszott idő: " + Math.floor(result.br.stats.pc.squad.minutesPlayed / 60) + " óra");
-			$("#all_minutesPlayed").html("Játszott idő: " + Math.floor(result.br.stats.pc.all.minutesPlayed / 60) + " óra");
-			
 			$("#solo_matchesPlayed").html("Játszott meccsek: " + result.br.stats.pc.solo.matchesPlayed);
 			$("#duo_matchesPlayed").html("Játszott meccsek: " + result.br.stats.pc.duo.matchesPlayed);
 			$("#squad_matchesPlayed").html("Játszott meccsek: " + result.br.stats.pc.squad.matchesPlayed);
@@ -63,11 +65,10 @@ function updatePlayer()
 			$("#squad_wins").html("Győzelmek: " + result.br.stats.pc.squad.wins + " (" + (result.br.stats.pc.squad.wins/result.br.stats.pc.squad.matchesPlayed*100).toFixed(2) + "%)");
 			$("#all_wins").html("Győzelmek: " + result.br.stats.pc.all.wins + " (" + (result.br.stats.pc.all.wins/result.br.stats.pc.all.matchesPlayed*100).toFixed(2) + "%)");
 			
-			
-			var solo_kpd = result.br.stats.pc.solo.kpd >= 1 ? "<span class='text-green'>" + (result.br.stats.pc.solo.kpd).toFixed(2) + "</span>" : "<span class='text-red'>" + (result.br.stats.pc.solo.kpd).toFixed(2) + "</span>";
-			var duo_kpd = result.br.stats.pc.duo.kpd >= 1 ? "<span class='text-green'>" + (result.br.stats.pc.duo.kpd).toFixed(2) + "</span>" : "<span class='text-red'>" + (result.br.stats.pc.duo.kpd).toFixed(2) + "</span>";
-			var squad_kpd = result.br.stats.pc.squad.kpd >= 1 ? "<span class='text-green'>" + (result.br.stats.pc.squad.kpd).toFixed(2) + "</span>" : "<span class='text-red'>" + (result.br.stats.pc.squad.kpd).toFixed(2) + "</span>";
-			var all_kpd = result.br.stats.pc.all.kpd >= 1 ? "<span class='text-green'>" + (result.br.stats.pc.all.kpd).toFixed(2) + "</span>" : "<span class='text-red'>" + (result.br.stats.pc.all.kpd).toFixed(2) + "</span>";
+			var solo_kpd = "<span class='text-" + getColorForKPD(result.br.stats.pc.solo.kpd) + "'>" + (result.br.stats.pc.solo.kpd).toFixed(2) + "</span>";			
+			var duo_kpd = "<span class='text-" + getColorForKPD(result.br.stats.pc.duo.kpd) + "'>" + (result.br.stats.pc.duo.kpd).toFixed(2) + "</span>";
+			var squad_kpd = "<span class='text-" + getColorForKPD(result.br.stats.pc.squad.kpd) + "'>" + (result.br.stats.pc.squad.kpd).toFixed(2) + "</span>";
+			var all_kpd = "<span class='text-" + getColorForKPD(result.br.stats.pc.all.kpd) + "'>" + (result.br.stats.pc.all.kpd).toFixed(2) + "</span>";
 			
 			$("#solo_kills").html("Ölések: " + result.br.stats.pc.solo.kills + " (K/D: " + solo_kpd + ")");
 			$("#duo_kills").html("Ölések: " + result.br.stats.pc.duo.kills + " (K/D: " + duo_kpd + ")");
@@ -75,6 +76,24 @@ function updatePlayer()
 			$("#all_kills").html("Ölések: " + result.br.stats.pc.all.kills + " (K/D: " + all_kpd + ")");
 		}
       });
+}
+
+function getColorForKPD(kpd)
+{
+	var color = "orange";
+	
+	if(kpd < 0.5)
+		color = "red";
+	if(0.5 <= kpd && kpd < 1)
+		color = "grey";
+	if(1 <= kpd && kpd < 1.5)
+		color = "green";
+	if(1.5 <= kpd && kpd < 2)
+		color = "blue";
+	if(2 <= kpd && kpd < 3)
+		color = "purple";
+	
+	return color;
 }
 
 function timeSince(date)
@@ -120,6 +139,68 @@ $(document).keypress(function(e)
     var keycode = (e.keyCode ? e.keyCode : e.which);
     if (keycode == '13')
 	{
-		updatePlayer();
+		updatePlayer("");
     }
 });
+
+function setCookie(name, value)
+{
+        var d = new Date();
+        d.setTime(d.getTime() + (365 * 24 * 60 * 60 * 1000));
+        var cookieExpireDate = "expires=" + d.toString();
+        document.cookie = name + "=" + value + "; " + cookieExpireDate + "; path=/";
+}
+
+function getCookie(cname)
+{
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function createCookiesIfNotExists()
+{
+	if(getCookie("player1") == "" && getCookie("player2") == "" && getCookie("player3") == "")
+	{
+		setCookie("player1", "");
+		setCookie("player2", "");
+		setCookie("player3", "");
+	}
+}
+
+function printLastPlayers()
+{
+	var toPrint = "";
+	
+	for(var i = 1; i <= 3; i++)
+	{
+		var player = getCookie("player" + i);
+		
+		if(player != "")
+		{
+			toPrint += "<button type='button' class='btn btn-dark ml-1' onClick='updatePlayer(\"" + player + "\");'>" + player + "</button>";
+		}
+	}
+	
+	$("#lastPlayers").html(toPrint);
+}
+
+function addLastPlayer(name)
+{
+	if(getCookie("player1") == name || getCookie("player2") == name || getCookie("player3") == name)
+		return;
+	
+	setCookie("player3", getCookie("player2"));
+	setCookie("player2", getCookie("player1"));
+	setCookie("player1", name);	
+}
